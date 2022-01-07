a855fd90ad1dc0e0a1259def700329156200e4eb
Ekin Bacik <ekinbacik@gmail.com>
2022-01-07 14:05:12 +0300

    new emoji sheet added/emoji keyboard added in this sheet

--- ToDoApp/AddNewTask.swift
+++ ToDoApp/AddNewTask.swift
@@ -4,7 +4,7 @@
 //
 //  Created by VB on 29.11.2021.
 //
-
+import BottomSheet_SwiftUI
 import SwiftUI
 struct AddNewTaskView: View {
     @Environment(\.presentationMode) var presentationMode
@@ -15,19 +15,22 @@ struct AddNewTaskView: View {
     @State var showAlert : Bool = false
     @State var EndDate = Date()
     @State var IsOk : Bool = false
+    @State var show = false
     var dateFormatter : DateFormatter{
         let dateFormat = DateFormatter()
         dateFormat.dateStyle = .short
         return dateFormat
     }
+    @State private var isSheetOpen = false
     var body: some View {
-        ScrollView{
             VStack{
+                HStack{
                 TextField("Add item 🖊", text : $userText)
                     .padding()
                     .background(Color.white.opacity(0.3 ))
                     .cornerRadius(10)
                     .frame(height:50)
+                }
                 if IsOk{
                     DatePicker("",selection: $EndDate, in:Date.now...,displayedComponents: .date)
                         .labelsHidden()
@@ -39,8 +42,6 @@ struct AddNewTaskView: View {
                     Text("Add a Date 📅")
                         .bold()
                 }.tint(Color("BackgroundColor"))
-               // Text("Endate")
-               // Text(dateFormatter.string(from: EndDate))
                 Button(action: saveButtonPressed, label: {
                                  Text("Save".uppercased())
                                      .foregroundColor(.white)
@@ -50,10 +51,15 @@ struct AddNewTaskView: View {
                                      .background(Color("BackgroundColor"))
                                      .cornerRadius(10)
                              })
+                BottomSheet(
+                              isOpen: self.$isSheetOpen,
+                              config: BottomSheetConfig(maxHeight: UIScreen.main.bounds.height/3)
+                          ){
+                              EmojiView(show: self.$show, userText: self.$userText)
+                              Color.white
+                          }
 
-            }
-        }
-        .padding(14)
+            }.padding(14)
         .navigationBarTitle("Add a new task")
         .alert(isPresented: $showAlert, content: alertCome)
         
@@ -84,6 +90,55 @@ struct AddNewTaskView: View {
     }
     func alertCome() -> Alert{
         return Alert(title: Text(alertText))
+    }
+}
+struct EmojiView: View {
+    @Binding var show : Bool
+    @Binding var userText : String
+    var body: some View{
+        ZStack(alignment:.topLeading){
+            ScrollView(.vertical, showsIndicators: false){
+                VStack(spacing:15){
+                    ForEach(self.getEmoji(),id:\.self){emoji in
+                        HStack(spacing:15){
+                        ForEach(emoji, id: \.self){j in
+                            Button {
+                                self.userText += String(UnicodeScalar(j)!)                             } label: {
+                                if (UnicodeScalar(j)?.properties.isEmoji)!{
+                                    Text(String(UnicodeScalar(j)!)).font(.system(size:55))
+                                }else{
+                                    Text("")
+                                }
+                            }
+                         }
+                        }
+                    }
+                }
+            }
+                .frame(width:UIScreen.main.bounds.width, height:UIScreen.main.bounds.height/3)
+                .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+                .background(Color.white)
+                .cornerRadius(25)
+            Button {
+                self.show.toggle()
+            } label: {
+               Text("")
+            }
+
+        }
+    }
+    func getEmoji()->[[Int]]{
+        var emojiArray : [[Int]] = []
+        
+        for emojiCodes in stride(from:0x1F601, to: 0x1F64F, by: 4){
+            var temp :[Int] = []
+            for j in emojiCodes...emojiCodes+3{
+                
+                temp.append(j)
+            }
+            emojiArray.append(temp)
+        }
+       return emojiArray
     }
 }
 
--- ToDoApp/Views/EmojiTestView.swift
+++ ToDoApp/Views/EmojiTestView.swift
@@ -5,45 +5,72 @@
 //  Created by VB on 6.01.2022.
 //
 
-import SwiftUI
+/*import SwiftUI
 
 struct EmojiTestView: View {
+    @State var show = false
+    @State var value = ""
     var body: some View {
-        ZStack(alignment:.topLeading){
+        ZStack(alignment:.bottom){
         GeometryReader{_ in
-            Text("")
+            VStack{
+                HStack(spacing:15){
+                    TextField("test", text: $value)
+                    Button {
+                        UIApplication.shared.windows.first?.rootViewController?.view.endEditing(true)
+                        self.show.toggle()
+                    } label: {
+                        Image(systemName: "smiley").foregroundColor(.gray)
+                    }
+                }.padding(13)
+                    .background(Color.white)
+                    .clipShape(Capsule())
+            }
         }
-        EmojiView()
+            EmojiView(show: self.$show, value: self.$value).offset(y: self.show ?  (UIApplication.shared.windows.first?.safeAreaInsets.bottom)! : UIScreen.main.bounds.height)
         }.background(Color.gray).edgesIgnoringSafeArea(.all)
     }
 }
 struct EmojiView: View {
+    @Binding var show : Bool
+    @Binding var value : String
     var body: some View{
-        ScrollView(.vertical, showsIndicators: false){
-            VStack(spacing:15){
-                ForEach(self.getEmoji(),id:\.self){emoji in
-                    HStack(spacing:15){
-                    ForEach(emoji, id: \.self){j in
-                        Button {
-                        } label: {
-                            if (UnicodeScalar(j)?.properties.isEmoji)!{
-                                Text(String(UnicodeScalar(j)!)).font(.system(size:55))
+        ZStack(alignment:.topLeading){
+            ScrollView(.vertical, showsIndicators: false){
+                VStack(spacing:15){
+                    ForEach(self.getEmoji(),id:\.self){emoji in
+                        HStack(spacing:15){
+                        ForEach(emoji, id: \.self){j in
+                            Button {
+                                self.value += String(UnicodeScalar(j)!)                             } label: {
+                                if (UnicodeScalar(j)?.properties.isEmoji)!{
+                                    Text(String(UnicodeScalar(j)!)).font(.system(size:55))
+                                }else{
+                                    Text("")
+                                }
                             }
+                         }
                         }
-                     }
                     }
                 }
+            }.frame(width:UIScreen.main.bounds.width, height:UIScreen.main.bounds.height/3)
+                .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+                .background(Color.white)
+                .cornerRadius(25)
+            Button {
+                self.show.toggle()
+            } label: {
+                Image(systemName: "xmark").foregroundColor(.red).padding()
             }
-        }.frame(width:UIScreen.main.bounds.width, height:UIScreen.main.bounds.height/3)
-            .background(Color.white)
-            .cornerRadius(25)
+
+        }
     }
     func getEmoji()->[[Int]]{
         var emojiArray : [[Int]] = []
         
-        for emojiCodes in 0x1F601...0x1F64F{
+        for emojiCodes in stride(from:0x1F601, to: 0x1F64F, by: 4){
             var temp :[Int] = []
-            for j in emojiCodes...emojiCodes+3{
+            for j in emojiCodes...emojiCodes+2{
                 
                 temp.append(j)
             }
@@ -57,3 +84,4 @@ struct EmojiTestView_Previews: PreviewProvider {
         EmojiTestView()
     }
 }
+*/
